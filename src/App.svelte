c779a9e1e38196630a33d8b276b7b17a9976e4c3
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2024-07-21 22:47:45 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import svelteLogo from './assets/svelte.svg'
+	import viteLogo from '/vite.svg'
+	import Counter from './lib/Counter.svelte'
+</script>
+    <div id="nescss">
+        <header>
+            <div class="container">
+                <div class="nav-brand">
+                    <a href="https://nostalgic-css.github.io/NES.css/"> <h1><i class="snes-jp-logo brand-logo"></i>NES.css</h1> </a>
+                    <p>NES-style CSS Framework.</p>
+                </div>
+                <div class="social-buttons">
+                    <p>Share on SNS</p>
+                    <div class="share">
+                        <a href="https://twitter.com/share?text=NES.css%EF%BD%9CNES-style%20CSS%20Framework%20%40bc_rikko&url=https://nostalgic-css.github.io/NES.css/" target="_blank"><i class="nes-icon twitter"></i></a>
+                        <a href="https://www.facebook.com/sharer.php?u=https://nostalgic-css.github.io/NES.css/&t=NES.css%EF%BD%9CNES-style%20CSS%20Framework" target="_blank"><i class="nes-icon facebook"></i></a>
+                        <a href="https://www.linkedin.com/shareArticle?url=https%3A//nostalgic-css.github.io/NES.css/&title=NES.css%EF%BD%9CNES-style%20CSS%20Framework" target="_blank"><i class="nes-icon linkedin"></i></a>
+                        <a href="https://github.com/nostalgic-css/NES.css" target="_blank"><i class="nes-icon github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            <button class="nes-btn is-primary">Primary</button>
+            <button class="pixel-button">Pixel Button</button>
+        </div>
+    </div>
